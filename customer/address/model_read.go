8a67d0d2aab54a22ce3684f52287a53ffb9a4eab
miftah <muhammadmiftahrizki@gmail.com>
2025-09-25 11:49:08 +0700

    added update customer address feature

--- customer/address/model_read.go
+++ customer/address/model_read.go
@@ -1,10 +1,10 @@
 package address
 
-type Address struct {
-	Id         uint8  `json:"id"`
+type AddressReadModel struct {
+	Id         int    `json:"id"`
 	Address    string `json:"address"`
 	Address2   string `json:"address_2"`
 	District   string `json:"district"`
-	CityId     uint8  `json:"city_id"`
+	CityId     int    `json:"city_id"`
 	PostalCode string `json:"postal_code"`
 }
--- customer/model_sql.go
+++ customer/model_sql.go
@@ -1,13 +1,18 @@
 package customer
 
-import "database/sql"
+import (
+	"database/sql"
 
-type CustomerSql struct {
+	"github.com/mmiftahrzki/customer/customer/address"
+)
+
+type customerSQLModel struct {
 	customer_id sql.NullInt16
 	first_name  sql.NullString
 	last_name   sql.NullString
 	email       sql.NullString
 	address_id  sql.NullInt16
+	address     address.AddressSqlModel
 	active      sql.NullBool
 	create_date sql.NullTime
 }
